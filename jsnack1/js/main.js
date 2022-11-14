abbf448bbeff3e396e0687118d11b8f8c093c7d1
Pedra96 <darkraidialga@yahoo.it>
2022-11-14 14:19:32 +0100

    fine jsnack1

--- jsnack1/js/main.js
+++ jsnack1/js/main.js
@@ -1,2 +1,23 @@
 /*L'utente inserisce due numeri in succesione,con due prompt.
 il software stampa il maggiore*/
+
+
+let inputuno;
+let inputdue;
+do{
+    inputuno= prompt("inserisci un numero");
+}while(isNaN(inputuno=parseInt(inputuno))== true);
+
+do{
+    inputdue= prompt("inserisci un altro numero");
+}while(isNaN(inputdue=parseInt(inputdue))== true);
+
+if(inputuno>inputdue){
+    console.log(inputuno+" è maggiore");
+}else if(inputuno<inputdue){
+    console.log(inputdue+" è maggiore");
+}else if(inputuno==inputdue){
+    console.log(inputdue+" sono uguali");
+}else{
+    console.log("Error unexpected behavior");
+}
